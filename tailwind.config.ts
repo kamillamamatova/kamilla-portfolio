0cce5933d141f57eab65559d4293da774976f198
kamillamamatova <kamillamamatova.km@gmail.com>
2025-12-20 00:03:50 -0500

    Added fade in animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,22 @@ const config: Config = {
                 sans: ["var(--font-sans)"],
                 serif: ["var(--font-serif)"],
             },
+
+            animation: {
+                'fade-in': 'fadeInUp 0.8s ease-out forwards',
+            },
+            keyFrames: {
+                fadeInUp: {
+                    '0%': {
+                        opacity: 0,
+                        transform: 'translateY(10px)',
+                    },
+                    '100%': {
+                        opacity: 1,
+                        transform: 'translateY(0)',
+                    },
+                },
+            },
         },
     },
     plugins: [],
