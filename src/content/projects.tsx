a5f0fb3ad3ef9ced93a7eb4a5bc0f1bbde508c65
kamillamamatova <kamillamamatova.km@gmail.com>
2025-12-21 00:01:16 -0500

    Filled in 2 projects

--- src/content/projects.tsx
+++ src/content/projects.tsx
@@ -11,23 +11,23 @@ export type Project = {
 
 export const projects: Project[] = [
     {
-        title: "Project One",
-        description: "I don't know. Stuff. Placeholder.",
-        tags: ["Next.js", "TypeScript", "Tailwind"],
+        title: "RocketTime",
+        description: "A full-stack time management and goal-setting application featuring a React dashboard, a Node.js API with Google OAuth, and an AI-powered chat assistant for managing tasks and time logs.",
+        tags: ["TypeScript", "Node.js", "React", "MySQL2", "Google OAuth", "Express", "Google APIs", "Vite"],
         links: [
-            { label: "Live", href: "https://"},
-            { label: "Code", href: "https://github.com/kamillamamatova/"},
+            //{ label: "Live", href: "https://"},
+            { label: "Code", href: "https://github.com/kamillamamatova/rocket-time"},
         ],
-        highlights: ["Role: solo", "Focus:"],
+        highlights: ["Role: Backend", "Focus:"],
     },
     {
-        title: "Project Two",
-        description: "...",
-        tags: ["Next.js", "TypeScript", "Tailwind"],
+        title: "AI Resume Analyzer",
+        description: "Users upload their resume and AI analyzes it and gives feedback on what they can improve.",
+        tags: ["Python", "Streamlit", "OpenAI API", "PyPDF2", "Pandas", "NumPy", "Matplotlib", "Altair", "Pillow"],
         links: [
-            { label: "Live", href: "https://"},
-            { label: "Code", href: "https://github.com/kamillamamatova/"},
+            { label: "Live", href: "https://ai-resume-analyzer-c9chdla4qiz6633ikqjdyd.streamlit.app/"},
+            { label: "Code", href: "https://github.com/kamillamamatova/ai-resume-analyzer"},
         ],
-        highlights: ["Role: solo", "Focus:"],
+        highlights: ["Role: Solo", "Focus:"],
     },
 ];
